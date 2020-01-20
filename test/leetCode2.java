a037ee425a6348f982a495f5207ab85c42703be4
zorba87 <59608468+zorba87@users.noreply.github.com>
2020-01-20 15:34:57 +0900

    Update leetCode2.java

--- test/leetCode2.java
+++ test/leetCode2.java
@@ -30,67 +30,67 @@
         }
         
         String value =  sb1.reverse().toString();
-		String value2 = sb2.reverse().toString();
-		
-		int length= value.length();
-		int length2= value2.length();
-		
-		int maxlength = (length > length2)? length: length2;
-        
-		boolean flag = false;
-		ArrayList<Integer> list = new ArrayList<>();
-		
-         if(length != maxlength){
-            int bufferNum = maxlength- length;
-            String buffer ="";
-            
-            for(int i=0; i<bufferNum; i++){
-                buffer+="0";            
-            }
-            sb1.insert(0,buffer);
+	String value2 = sb2.reverse().toString();
 
-         }
-        if(length2 != maxlength){
-            int bufferNum = maxlength- length2;
-            String buffer ="";
+	int length= value.length();
+	int length2= value2.length();
+
+	int maxlength = (length > length2)? length: length2;
+
+	boolean flag = false;
+	ArrayList<Integer> list = new ArrayList<>();
+		
+        if(length != maxlength){
+        	int bufferNum = maxlength- length;
+        	String buffer ="";
             
-            for(int i=0; i<bufferNum; i++){
-                buffer+="0";            
-            }
-            sb2.insert(0,buffer);
+		for(int i=0; i<bufferNum; i++){
+			buffer+="0";            
+		}
+		sb1.insert(0,buffer);
+        }
+	    
+        if(length2 != maxlength){
+		int bufferNum = maxlength- length2;
+		String buffer ="";
+
+		for(int i=0; i<bufferNum; i++){
+			buffer+="0";            
+		}
+		sb2.insert(0,buffer);
         }
         
-		for (int i = maxlength-1; i >= 0 ; i--) {
-			int sum = 0;
-			int part=0,part2=0;
+	for (int i = maxlength-1; i >= 0 ; i--) {
+		int sum = 0;
+		int part=0,part2=0;
 			
-            part =  sb1.toString().charAt(i)-'0';	
-            part2 = sb2.toString().charAt(i)-'0';	
+           	part =  sb1.toString().charAt(i)-'0';	
+          	part2 = sb2.toString().charAt(i)-'0';	
             
-            sum = part+part2;
+            	sum = part+part2;
 			
-			if(flag) {
-                sum +=1;
-                flag = false;
-			}
-            
-			if(sum >= 10) {
-				list.add(sum-10);
-				flag = true;
-			}else {
-				list.add(sum);
-			}
+		if(flag) {
+               		sum +=1;
+			flag = false;
 		}
+            
+		if(sum >= 10) {
+			list.add(sum-10);
+			flag = true;
+		}else {
+			list.add(sum);
+		}
+	}
         
         if(flag) {
 			list.add(1);
-		}
+	}
         
         ListNode[] node = new ListNode[list.size()];
 
         for (int i = 0; i<list.size() ; i++) {
             node[i] = new ListNode(list.get(i));
-		}
+	}
         
         for (int i = 0; i<list.size() ; i++) {
             if(i != list.size()-1){

a44a3fe1c726de006e866c46e61b31deb25521cd
glshlee <glshlee@gmail.com>
2020-05-20 01:16:10 +0900

    solve fizz-buzz

---
+++ fizz-buzz/Solution.py
@@ -0,0 +1,19 @@
+from typing import List
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        result = []
+        for num in range(1, n+1):
+            if num % (3*5) == 0:
+                result.append("FizzBuzz")
+            elif num % 3 == 0:
+                result.append("Fizz")
+            elif num % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(num))
+        return result
+            
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.fizzBuzz(15))
